6b4a1838572d862a1a17179ddbc771fe4aa673b3
Martin <martin.takats@t-online.de>
2026-01-22 18:36:07 +0100

    Switch to Orin Nano client next steps

--- src/main/java/org/example/cobraflex/UiController.java
+++ src/main/java/org/example/cobraflex/UiController.java
@@ -22,25 +22,24 @@ import org.example.cobraflex.clients.SpeedLevel;
 public class UiController {
 
   @FXML
-  private TextField bf_odl;
+  public TextField bf_odl;
   @FXML
-  private TextField bf_odr;
+  public TextField bf_odr;
   @FXML
-  private TextField bf_voltage;
+  public TextField bf_voltage;
   @FXML
-  private TextArea console;
+  public TextArea console;
 
   @Setter
   private Stage stage;
   private ESP32S3Client esp32;
-  private JetsonOrinNanoClient jetson;
   private CobraFlexClient cobraflex;
   private KeyboardController keyboardController;
   private Timer gimbalTimer = null;
   private Timer chassisTimer = null;
 
   @FXML
-  private void initialize() {
+  public void initialize() {
     this.esp32 = new ESP32S3Client();
     this.cobraflex = new CobraFlexClient();
     keyboardController = new KeyboardController(esp32, cobraflex);
@@ -56,157 +55,157 @@ public class UiController {
   }
 
   @FXML
-  private void getFeedback() {
+  public void getFeedback() {
     String cmd = cobraflex.cmd_feedback();
     JsonNode result = esp32.get(cmd);
     bf_odl.setText(getParamValue("odl", result));
     bf_odr.setText(getParamValue("odr", result));
     bf_voltage.setText(roundParamValue("v", result));
-    console.appendText(result.toString() + "\n");
+    console.appendText(result + "\n");
   }
 
   // gimbal upper left button
   @FXML
-  private void gul_pressed() {
+  public void gul_pressed() {
     repeat_gimbal_cmd(-1, 1);
   }
 
   // gimbal upper middle button
   @FXML
-  private void gum_pressed() {
+  public void gum_pressed() {
     repeat_gimbal_cmd(0, 1);
   }
 
   // gimbal upper right button
   @FXML
-  private void gur_pressed() {
+  public void gur_pressed() {
     repeat_gimbal_cmd(1, 1);
   }
 
   // gimbal middle left button
   @FXML
-  private void gml_pressed() {
+  public void gml_pressed() {
     repeat_gimbal_cmd(-1, 0);
   }
 
   // gimbal middle middle button
   @FXML
-  private void gmm_pressed() {
+  public void gmm_pressed() {
     String cmd = cobraflex.cmd_gimbal_ctrl_simple(0, 0);
     esp32.get(cmd);
   }
 
   // gimbal middle right button
   @FXML
-  private void gmr_pressed() {
+  public void gmr_pressed() {
     repeat_gimbal_cmd(1, 0);
   }
 
   // gimbal bottom left button
   @FXML
-  private void gbl_pressed() {
+  public void gbl_pressed() {
     repeat_gimbal_cmd(-1, -1);
   }
 
   // gimbal bottom middle button
   @FXML
-  private void gbm_pressed() {
+  public void gbm_pressed() {
     repeat_gimbal_cmd(0, -1);
   }
 
   // gimbal bottom right button
   @FXML
-  private void gbr_pressed() {
+  public void gbr_pressed() {
     repeat_gimbal_cmd(1, -1);
   }
 
   // chassis upper left button
   @FXML
-  private void cul_pressed() {
+  public void cul_pressed() {
     repeat_chassis_cmd(MovingDirection.NORTHWEST);
   }
 
   // chassis upper middle button
   @FXML
-  private void cum_pressed() {
+  public void cum_pressed() {
     repeat_chassis_cmd(MovingDirection.NORTH);
   }
 
   // chassis upper right button
   @FXML
-  private void cur_pressed() {
+  public void cur_pressed() {
     repeat_chassis_cmd(MovingDirection.NORTHEAST);
   }
 
   // chassis middle left button
   @FXML
-  private void cml_pressed() {
+  public void cml_pressed() {
     repeat_chassis_cmd(MovingDirection.WEST);
   }
 
   // chassis middle middle button
   @FXML
-  private void cmm_pressed() {
+  public void cmm_pressed() {
     String cmd = cobraflex.cmd_speed_control(MovingDirection.STOP);
     esp32.get(cmd);
   }
 
   // chassis middle right button
   @FXML
-  private void cmr_pressed() {
+  public void cmr_pressed() {
     repeat_chassis_cmd(MovingDirection.EAST);
   }
 
   // chassis bottom left button
   @FXML
-  private void cbl_pressed() {
+  public void cbl_pressed() {
     repeat_chassis_cmd(MovingDirection.SOUTHWEST);
   }
 
   // chassis bottom middle button
   @FXML
-  private void cbm_pressed() {
+  public void cbm_pressed() {
     repeat_chassis_cmd(MovingDirection.SOUTH);
   }
 
   // chassis bottom right button
   @FXML
-  private void cbr_pressed() {
+  public void cbr_pressed() {
     repeat_chassis_cmd(MovingDirection.SOUTHEAST);
   }
 
   @FXML
-  private void radio_button4() {
+  public void radio_button4() {
     cobraflex.setSpeed(SpeedLevel.LEVEL_FOUR);
   }
 
   @FXML
-  private void radio_button3() {
+  public void radio_button3() {
     cobraflex.setSpeed(SpeedLevel.LEVEL_THREE);
   }
 
   @FXML
-  private void radio_button2() {
+  public void radio_button2() {
     cobraflex.setSpeed(SpeedLevel.LEVEL_TWO);
   }
 
   @FXML
-  private void radio_button1() {
+  public void radio_button1() {
     cobraflex.setSpeed(SpeedLevel.LEVEL_ONE);
   }
 
   @FXML
-  protected void keyPressed(KeyEvent event) {
+  public void keyPressed(KeyEvent event) {
     keyboardController.keyPressed(event);
   }
 
   @FXML
-  protected void keyReleased(KeyEvent event) {
+  public void keyReleased(KeyEvent event) {
     keyboardController.keyReleased(event);
   }
 
   @FXML
-  protected void enterKeyboardControl() {
+  public void enterKeyboardControl() {
     // focus on button is sufficient
   }
 
@@ -241,7 +240,7 @@ public class UiController {
   }
 
   @FXML
-  private void gimbal_released() {
+  public void gimbal_released() {
     gimbalTimer.cancel();
     String cmd = cobraflex.cmd_gimbal_ctrl_stop();
     esp32.get(cmd);
@@ -262,7 +261,7 @@ public class UiController {
   }
 
   @FXML
-  private void chassis_released() {
+  public void chassis_released() {
     chassisTimer.cancel();
     String cmd = cobraflex.cmd_speed_control(MovingDirection.STOP);
     esp32.get(cmd);
